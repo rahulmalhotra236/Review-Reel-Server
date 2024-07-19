bf4dbdac7fdc2396b4035d75f04a059f8fd9b98b
Rahul Malhotra <rahulmalhotra236@gmail.com>
2024-07-20 00:07:21 +0530

    add signin backend

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,8 +1,38 @@
 import Auth from "../models/auth.models.js"
 import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
 
+// const signinController = async (req, res) => {
+//   const { email, password } = req.body
+// }
 const signinController = async (req, res) => {
   const { email, password } = req.body
+
+  try {
+    // Check if user exists
+    const user = await Auth.findOne({ email })
+    if (!user) {
+      return res.status(400).json({ message: "Invalid email or password" })
+    }
+
+    // Check if password is correct
+    const isMatch = await bcrypt.compare(password, user.password)
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid email or password" })
+    }
+
+    // Generate JWT token
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    })
+
+    // Send response
+    res.cookie("token", token, { httpOnly: true })
+    return res.status(200).json({ message: "Sign-in successful", token })
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ message: "Server error" })
+  }
 }
 
 const signupController = async (req, res) => {
